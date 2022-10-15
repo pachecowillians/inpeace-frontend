29bae886343b80db5acc2d26c8c2e0c7beccd2a2
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-10-15 10:22:21 -0300

    Creating cards dynamically

--- scripts/lista-usuarios.js
+++ scripts/lista-usuarios.js
@@ -4,30 +4,37 @@ async function fetchUsers() {
     return jsonResponse;
 }
 
-/* <div class="user">
-                <button>
-                    <img src="../assets/icon-edit.svg" alt="Edit icon">
-                </button>
-                <div class="picture-name">
-                    <img src="https://reqres.in/img/faces/9-image.jpg" alt="Profile picture">
-                    <span>John Doe</span>
-                </div>
-                <span>johndoe@uppersoft.com</span>
-            </div> */
-
 function createCard(user) {
     console.log(user);
     let divUser = document.createElement("div");
-    divUser.innerHTML = user.first_name;
     divUser.classList.add('user');
 
     let button = document.createElement("button");
-    let profilePicture = document.createElement("img");
-    profilePicture.src = "../assets/icon-edit.svg";
-    profilePicture.alt = "Edit user";
+    let editIcon = document.createElement("img");
+    editIcon.src = "../assets/icon-edit.svg";
+    editIcon.alt = "Edit user";
 
-    button.appendChild(profilePicture);
-    divUser.appendChild(button)
+    button.appendChild(editIcon);
+
+    let pictureName = document.createElement("div");
+    pictureName.classList.add("picture-name");
+
+    let avatar = document.createElement("img");
+    avatar.src = user.avatar;
+    avatar.alt = "Avatar";
+
+    let name = document.createElement("span");
+    name.innerHTML = user.first_name + " " + user.last_name;
+
+    pictureName.appendChild(avatar);
+    pictureName.appendChild(name);
+
+    let mail = document.createElement("span");
+    mail.innerHTML = user.email;
+
+    divUser.appendChild(button);
+    divUser.appendChild(pictureName);
+    divUser.appendChild(mail);
     return divUser;
 }
 
