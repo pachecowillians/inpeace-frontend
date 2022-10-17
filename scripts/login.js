27c38d8def64276215edea7f4039202a37c6c2cc
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-10-17 13:18:54 -0300

    Validating if all the fields are filled

--- scripts/login.js
+++ scripts/login.js
@@ -9,19 +9,24 @@ function handleSubmit(event) {
         var formData = new FormData(form);
         let formDataObject = Object.fromEntries(formData);
 
-        // Getting email and password from sessionStorage
-        let validLogin = JSON.parse(sessionStorage.getItem("login"));
-
-        // If the email and password matches the data stored on sessionStorage
-        if (formDataObject.email === validLogin.email && formDataObject.password === validLogin.password) {
-            window.location.assign("./lista-usuarios.html");
+        // Validating if all the fields are filled
+        if (formDataObject["email"].trim().length == 0 || formDataObject["password"].trim().length == 0) {
+            alert("Preencha todos os campos!");
         } else {
-            // Wrong email or password
-            alert("E-mail ou senha inválidos!");
+            // Getting email and password from sessionStorage
+            let validLogin = JSON.parse(sessionStorage.getItem("login"));
 
-            // Cleaning the fields
-            document.querySelector("#email").value = '';
-            document.querySelector("#password").value = '';
+            // If the email and password matches the data stored on sessionStorage
+            if (formDataObject.email === validLogin.email && formDataObject.password === validLogin.password) {
+                window.location.assign("./lista-usuarios.html");
+            } else {
+                // Wrong email or password
+                alert("E-mail ou senha inválidos!");
+
+                // Cleaning the fields
+                document.querySelector("#email").value = '';
+                document.querySelector("#password").value = '';
+            }
         }
     } catch (error) {
         alert("Erro ao submeter o formulário");
