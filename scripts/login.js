2878e673a6d3149a833c29c6fa54592d55afbe4c
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-10-17 13:06:19 -0300

    Commenting the code and using try catch

--- scripts/login.js
+++ scripts/login.js
@@ -1,18 +1,30 @@
+// Function that executes when the form is submitted
 function handleSubmit(event) {
+    // Prevent form to redirect the page
     event.preventDefault();
 
-    let form = event.target;
-    var formData = new FormData(form);
-    let formDataObject = Object.fromEntries(formData);
+    try {
+        // Getting form data
+        let form = event.target;
+        var formData = new FormData(form);
+        let formDataObject = Object.fromEntries(formData);
 
-    let validLogin = JSON.parse(sessionStorage.getItem("login"));
+        // Getting email and password from sessionStorage
+        let validLogin = JSON.parse(sessionStorage.getItem("login"));
 
-    if (formDataObject.email === validLogin.email && formDataObject.password === validLogin.password) {
-        window.location.assign("./lista-usuarios.html");
-    } else {
-        alert("E-mail ou senha inválidos!");
-        document.querySelector("#email").value = '';
-        document.querySelector("#password").value = '';
+        // If the email and password matches the data stored on sessionStorage
+        if (formDataObject.email === validLogin.email && formDataObject.password === validLogin.password) {
+            window.location.assign("./lista-usuarios.html");
+        } else {
+            // Wrong email or password
+            alert("E-mail ou senha inválidos!");
+
+            // Cleaning the fields
+            document.querySelector("#email").value = '';
+            document.querySelector("#password").value = '';
+        }
+    } catch (error) {
+        alert("Erro ao submeter o formulário");
+        console.log(error);
     }
-
 }
