865ea061f06285abcd142c8419b388911c2ced38
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-10-17 13:02:55 -0300

    Commenting the code and using try catch

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,32 +1,44 @@
+// Function that executes when user submits the form
 function handleSubmit(event) {
+    // Prevent to redirect page
     event.preventDefault();
 
-    let form = event.target;
-    var formData = new FormData(form);
-    let formDataObject = Object.fromEntries(formData);
+    try {
+        // Getting form data
+        let form = event.target;
+        var formData = new FormData(form);
+        let formDataObject = Object.fromEntries(formData);
 
-    if (formDataObject["sex"] === undefined ||
-        formDataObject["state"] === undefined ||
-        formDataObject["name"].trim().length == 0 ||
-        formDataObject["email"].trim().length == 0 ||
-        formDataObject["password"].trim().length == 0 ||
-        formDataObject["confirm-password"].trim().length == 0) {
-        alert("Preencha todos os campos!");
-    } else if (formDataObject["password"] === formDataObject["confirm-password"]) {
+        // Validating empty fields
+        if (formDataObject["sex"] === undefined ||
+            formDataObject["state"] === undefined ||
+            formDataObject["name"].trim().length == 0 ||
+            formDataObject["email"].trim().length == 0 ||
+            formDataObject["password"].trim().length == 0 ||
+            formDataObject["confirm-password"].trim().length == 0) {
+            alert("Preencha todos os campos!");
+        } else if (formDataObject["password"] === formDataObject["confirm-password"]) {
+            // Validating equality between password and confirm password
+            let loginObject = {
+                "email": formDataObject.email,
+                "password": formDataObject.password,
+            }
 
-        let loginObject = {
-            "email": formDataObject.email,
-            "password": formDataObject.password,
+            // Saving email and password on sessionStorage
+            sessionStorage.setItem("login", JSON.stringify(loginObject));
+
+            alert("Cadastro realizado com sucesso!");
+
+            // Redirecting to login page
+            window.location.assign("./index.html");
+        } else {
+            // When password and confirm password are different
+            alert("As senhas não coincidem. Por favor, defina-as novamente.");
+            document.querySelector("#password").value = '';
+            document.querySelector("#confirm-password").value = '';
         }
-
-        sessionStorage.setItem("login", JSON.stringify(loginObject));
-
-        alert("Cadastro realizado com sucesso!");
-
-        window.location.assign("./index.html");
-    } else {
-        alert("As senhas não coincidem. Por favor, defina-as novamente.");
-        document.querySelector("#password").value = '';
-        document.querySelector("#confirm-password").value = '';
+    } catch (error) {
+        alert("Error on submit form");
+        console.log(error);
     }
 }
